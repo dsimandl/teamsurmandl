e886d5a3454b5c80f749bc8dc83e7f7b8b89a684
David Simandl <david.simandl@gmail.com>
2013-11-19 14:37:36 -0500

    Fixed the edit post url route

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from .views import PostListView, PostDetailView, PostEditView, PostCreateView
 urlpatterns = patterns('',
                        url(r'^$', PostListView.as_view(), name="list"),
                        url(r'^create/$', PostCreateView.as_view(), name='create_post'),
-                       url(r"^edit/(?P<pk>\d+)$", PostEditView.as_view(), name="edit_post"),
-                       url(r"^(?P<slug>[\w-]+)$", PostDetailView.as_view(), name="detail"),
+                       url(r'^edit/blog/(?P<slug>[\w-]+)$', PostEditView.as_view(), name='edit_posts'),
+                       url(r'^(?P<slug>[\w-]+)$', PostDetailView.as_view(), name='detail'),
 )
