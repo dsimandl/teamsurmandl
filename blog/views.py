79c3ee1ebc7fcb1e300c42addc27320382a55ce1
David Simandl <david.simandl@gmail.com>
2013-11-19 14:38:10 -0500

    Added blog content to the main site

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import UpdateView, ListView, FormView
+from django.views.generic import UpdateView, ListView, FormView, DetailView
+from django.views.generic.edit import FormMixin
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
@@ -7,7 +8,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.template.defaultfilters import slugify
 
 from .models import Post
-from .forms import PostCreateForm
+from .forms import PostCreateForm, PostViewForm
 
 class PublishedPostMixin(object):
 
@@ -19,12 +20,11 @@ class PostListView(PublishedPostMixin, ListView):
 
     model = Post
 
-class PostDetailView(PublishedPostMixin, UpdateView):
+class PostDetailView(PublishedPostMixin, DetailView, FormMixin):
 
     model = Post
-    form_class = PostCreateForm
-    template_name_update = 'blog/post_create_update.html'
-    template_name_detail = 'blog/post_detail.html'
+    form_class = PostViewForm
+    template_name = 'blog/post_detail.html'
     success_url = '/blog/'
 
     @method_decorator(csrf_protect)
@@ -35,33 +35,6 @@ class PostDetailView(PublishedPostMixin, UpdateView):
     def form_valid(self, form):
         self.object = form.save()
         return HttpResponseRedirect(self.get_success_url())
-
-    def get(self, request, *args, **kwargs):
-
-        if request.user.is_staff:
-            self.object = self.get_object()
-            form_class = self.get_form_class()
-            form = self.get_form(form_class)
-            return self.render_to_response(self.get_context_data(object=self.object, form=form))
-        else:
-            self.object= self.get_object()
-            context = self.get_context_data(object=self.object)
-            return self.render_to_response(context)
-
-    def get_template_names(self):
-
-        if self.request.user.is_staff:
-            if self.template_name_update is None:
-                raise ImproperlyConfigured("TemplateResponseMixin requires either a definition of "
-                    "'template_name' or an implementation of 'get_template_names()'")
-            else:
-                return [self.template_name_update]
-        else:
-            if self.template_name_detail is None:
-                raise ImproperlyConfigured("TemplateResponseMixin requires either a definition of "
-                    "'template_name' or an implementation of 'get_template_names()'")
-            else:
-                return [self.template_name_detail]
 
 class PostCreateView(FormView):
 
@@ -82,3 +55,16 @@ class PostCreateView(FormView):
 class PostEditView(PublishedPostMixin, UpdateView):
 
     model = Post
+    template_name = 'blog/post_create_update.html'
+    form_class = PostCreateForm
+    success_url = "/blog/"
+
+    @method_decorator(csrf_protect)
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(PostEditView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        self.object = form.save()
+        return HttpResponseRedirect(self.get_success_url())
+
