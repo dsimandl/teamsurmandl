57a66f66ca7d5108c229185cc3194065d6b5f942
David Simandl <david.simandl@gmail.com>
2013-11-22 11:54:54 -0500

    Removed the old PostComment form

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,4 @@
 from django import forms
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, HTML, Field, Submit
-from crispy_forms.bootstrap import FormActions
 
 from .models import Post, PostComment
 
@@ -12,11 +9,5 @@ class PostCreateForm(forms.ModelForm):
         model = Post
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': '6'})
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': '6'}),
         }
-
-class PostViewForm(forms.ModelForm):
-
-    class Meta:
-
-        model = PostComment
