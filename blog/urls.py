a2cb100213cb7983304af70b11659ff1cbd848c2
David Simandl <david.simandl@gmail.com>
2013-11-22 11:56:07 -0500

    Removed the edit route, made details the view and edit route for the posts and post comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import PostListView, PostDetailView, PostEditView, PostCreateView
+from .views import PostListView, PostEditView, PostCreateView
 
 urlpatterns = patterns('',
                        url(r'^$', PostListView.as_view(), name="list"),
                        url(r'^create/$', PostCreateView.as_view(), name='create_post'),
-                       url(r'^edit/blog/(?P<slug>[\w-]+)$', PostEditView.as_view(), name='edit_posts'),
-                       url(r'^(?P<slug>[\w-]+)$', PostDetailView.as_view(), name='detail'),
+                       url(r'^(?P<slug>[\w-]+)$', PostEditView.as_view(), name='detail'),
 )
