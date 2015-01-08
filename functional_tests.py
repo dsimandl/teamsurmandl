1239857eff09cf0987675c519fa47e1e8dc03a27
David Simandl <david.simandl@gmail.com>
2015-01-08 16:52:59 -0500

    Built out the test a bit more for the user login.  Now uses unittest!

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+class VisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Surmandl' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_user_login(self):
+        self.browser.get('http://127.0.0.1:8000')
+        self.assertIn('Surmandl', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
