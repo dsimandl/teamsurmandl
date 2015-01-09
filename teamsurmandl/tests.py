c25326a030ef6c219a0ab8ee39ac71228f1414fe
David Simandl <david.simandl@gmail.com>
2015-01-09 12:42:52 -0500

    Added passing root url resolver test to login page

--- teamsurmandl/tests.py
+++ teamsurmandl/tests.py
@@ -1,6 +1,9 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from teamsurmandl.views import LoginView
 
-class SmokeTest(TestCase):
+class LoginPageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 2, 7)
+    def test_root_url_resolves_to_logon_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func.func_name, LoginView.__name__)
