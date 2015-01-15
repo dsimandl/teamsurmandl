563c99233dbce14de3599be24e679bbf9cec8304
David Simandl <david.simandl@gmail.com>
2015-01-15 17:47:18 -0500

    Changed to test that our response is an instance of an HTTPResponseForbidden object

--- teamsurmandl/tests.py
+++ teamsurmandl/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.http.response import HttpResponseForbidden
 from teamsurmandl.views import LoginView
 
 class LoginPageTest(TestCase):
@@ -12,6 +13,4 @@ class LoginPageTest(TestCase):
     def test_login_page_returns_correct_html(self):
         request = HttpRequest()
         response = LoginView.as_view()(request)
-        self.assertTrue(response.content.startswith('\n<!DOCTYPE html>'))
-        self.assertIn('<title>403 Forbidden</title>', response.content)
-        self.assertTrue(response.content.endswith('</html>\n'))
+        self.assertTrue(isinstance(response, HttpResponseForbidden))
