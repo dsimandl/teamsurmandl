9c85959a976bd041e7209af40d49086c5c82e643
David Simandl <david.simandl@gmail.com>
2015-01-15 18:20:17 -0500

    added unit test to check for HTTPRequestForbidden reponse for the current location page

--- location/tests.py
+++ location/tests.py
@@ -1,6 +1,16 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.http.response import HttpResponseForbidden
+from location.views import CurrentLocation
 
-class SmokeTest(TestCase):
+class CurrentLocationPageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_logon_page_view(self):
+        found = resolve('/location/current_location/')
+        self.assertEqual(found.func.func_name, CurrentLocation.__name__)
+
+    def test_login_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = CurrentLocation.as_view()(request)
+        self.assertTrue(isinstance(response, HttpResponseForbidden))
