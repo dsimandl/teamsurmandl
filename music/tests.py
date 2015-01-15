56c9aadfbf6f7e2ec889b5bdb3699e9a2484fb8e
David Simandl <david.simandl@gmail.com>
2015-01-15 18:18:29 -0500

    added unit test to check for HTTPRequestForbidden reponse for the music home page

--- music/tests.py
+++ music/tests.py
@@ -1,6 +1,16 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.http.response import HttpResponseForbidden
+from music.views import MusicView
 
-class SmokeTest(TestCase):
+class MusicPageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 5)
+    def test_root_url_resolves_to_logon_page_view(self):
+        found = resolve('/music')
+        self.assertEqual(found.func.func_name, MusicView.__name__)
+
+    def test_login_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = MusicView.as_view()(request)
+        self.assertTrue(isinstance(response, HttpResponseForbidden))
